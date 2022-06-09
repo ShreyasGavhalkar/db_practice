9e45de19bdb9f2fad19224d8a6eab877b1c565a0
Shreyas Gavhalkar <shreyasgavhalkar10@gmail.com>
2022-06-09 20:36:01 +0530

    started exploring jinja templates. This commit contains basic code that i wrote whilte exploring the engine.

---
+++ jinja_start.py
@@ -0,0 +1,17 @@
+from jinja2 import Template
+
+name = input("Enter name: ")
+age = int(input("Enter age: "))
+
+t = Template("Hello {{name}} you are {{age}} years old")
+
+m = t.render(name=name, age=age)
+
+print(m)
+
+print("\n\n")
+
+l = [1, {"shreyas": "gavhalkar", "vishwakarma": "college"}, 1.4, True]
+with open("practice.html.jinja") as f:
+    tmp = Template(f.read())
+print(tmp.render(l=l))
