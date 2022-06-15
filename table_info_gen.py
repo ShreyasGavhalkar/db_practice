f6cd5c21c2875e1802d53fc3009295cf44fa74d7
Shreyas Gavhalkar <shreyasgavhalkar10@gmail.com>
2022-06-15 17:20:01 +0530

    Added code to extract primary keys of tables

--- table_info_gen.py
+++ table_info_gen.py
@@ -1,6 +1,6 @@
 from jinja2 import Template
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine, MetaData, inspect
+from sqlalchemy import create_engine, MetaData, inspect, Table
 import webbrowser
 
 Base = declarative_base()
@@ -27,6 +27,12 @@ with open("templates/tables_template.html.jinja") as f:
         out = t.render(table=i, cols=cols)
         with open("output_pages/{}.html".format(i), "w") as o:
             o.write(out)
+        tb=Table(i, metadata_obj, autoload=True, autoload_with=engine)
+        primary_key=tb.primary_key.columns.values()
+        # print(primary_key)
+        for j in primary_key:
+            print(j.name, end=' ')
+        print()
 
 
 with open("output_pages/output.html", "w") as f:
